defe461e63996bc95d058d66ccbe675d0ae80a36
Adrien BAILLY <99869576+AdryBailly@users.noreply.github.com>
2025-04-20 09:47:08 +0200

    Update update_readme_by_city.py

--- .github/scripts/update_readme_by_city.py
+++ .github/scripts/update_readme_by_city.py
@@ -1,10 +1,6 @@
 
 ## Les offres classées par ville. (Mise à jour automatique via scripts & workflow)
 
-Full remote = 🏠
-Recrutement actif en cours (Nous avons un contrat) = 🔥
-Recrutement au profil (Nous avons un contrat) = ✨
-
 for ville in sorted(offres_par_ville_stack):
     section += f'\n### {ville}\n'
     for stack in sorted(offres_par_ville_stack[ville]):
